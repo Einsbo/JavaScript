7dd628e9e4020bba86bceea440116bf87b48d844
Eins <m18753168203@gmail.com>
2023-07-17 23:10:24 +0800

    Mon Jul 17 23:10

---
+++ 5. Objects, classes and object-oriented programming/Create object.js
@@ -0,0 +1,351 @@
+/**
+ * 使用 Object 构造函数或对象字面量创建对象有个明显的缺点：创建具有同样接口的多个对象需要重复便携很多代码
+ */
+
+// ! 工厂模式 —— 解决了创建多个类似对象的问题，但没有解决对象标识问题（即新创建的对象是什么类型）
+if (!true) {
+  /* createPerson() 接收 3 个参数并构建了一个包含 Person 信息的对象。可以用不同的参数多次调用这个函数，每次都会
+    返回包含 3 个属性和 1 个方法的对象。*/
+  function createPerson(name, age, job) {
+    let o = new Object();
+    o.name = name;
+    o.age = age;
+    o.job = job;
+    o.sayName = function() {
+      console.log(this.name);
+    };
+    return o;
+  }
+
+  let person1 = createPerson("Nicholas", 29, "Software engineer");
+  let person2 = createPerson("Greg", 27, "Doctor");
+
+  console.log(person1);
+}
+
+// ! 构造函数模式 —— 以函数的形式为自己的对象类型定义属性和方法，可以确保实例被标识为特定类型
+if (!true) {
+  /* 这里 Person() 构造函数代替了 createPerson() 工厂函数，内部的代码和上面基本一样，只是有如下区别：
+    1. 没有显示的创建
+    2. 属性和方法直接赋值给了 this
+    3. 没有 return
+     要创建 Person 实例应该使用 new 操作符，以这种方式调用构造函数会执行如下操作：
+    1. 在内存中创建一个新对象
+    2. 这个新对象内部的 [[Prototype]] 特性被赋值为构造函数的 prototype 属性
+    3. 构造函数内部的 this 被赋值为这个新对象
+    4. 执行构造函数内部的代码（给新对象添加属性）
+    5. 如果构造函数返回非空对象，则返回该对象；否则返回刚创建的新对象
+  */
+  function Person(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+    this.sayName = function() {
+      console.log(this.name);
+    };
+  }
+
+  let person1 = new Person("Nicholas", 29, "Software Engineer");
+  let person2 = new Person("Greg", 27, "Doctor");
+  // * person1 和 person2 分别保存着 Person 的不同实例，这两个对象的 constructor 都指向 Person 
+  console.log(person1.constructor == Person); // true
+
+  //* constructor 本来适用于标识对象类型的，不过一般认为 instanceof 操作符是确定对象类型更可靠的方式
+  console.log(person1 instanceof Object); // true
+  console.log(person1 instanceof Person); // true
+
+  person1.sayName(); // "Nicholas"
+  person2.sayName(); // "Greg"
+
+  // * 构造函数也是函数
+  /* 构造函数与普通函数唯一的区别就是调用方式不同。并没有把某个函数定义为构造函数的特殊语法。任何函数只要使用 new 操作
+    符调用就是构造函数，而不使用 new 操作符调用的函数就是普通函数。 */
+  // 作为构造函数
+  let person = new Person("Nicholas", 29, "Software Engineer");
+  person1.sayName(); // "Nicholas"
+
+  // 作为函数调用
+  Person("Killer", 27, "Doctor"); // 添加到 window 对象
+  window.sayName(); // "killer"
+
+  // 在另一个对象的作用域中调用
+  let o = new Object();
+  Person.call(o, "Kristen", 25, "Nurse"); // 将 o 指定为 Person() 内部的 this 值
+  o.sayName(); // "Kristen"
+
+  // ? 构造函数的问题
+  /* 构造函数的主要问题在于其定义的方法会在每个实例上都创建一遍，因此对前面的例子而言，person1 和 person2 都有名为
+    sayName() 的方法，但这两个方法不是同一个 Function 实例。ES 中的函数是对象，因此每次定义函数时都会初始化一个对
+    象。逻辑上讲这个构造函数实际上是这样的：
+      function Person(name, age, job) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.sayName = new Function("console.log(this.name)"); // 逻辑等价
+      } 
+  */
+  // 这代表实例上的函数虽然同名但不相等
+  console.log(person1.sayName === person2.sayName); // false
+
+  /* 因为都是做同样的事，所以没必要定义两个不同的 Function 实例，况且 this 对象可以把函数与对象的绑定推迟到运行时。
+    要解决这个问题，可以把函数定义转移到构造函数外部： */
+  function Person(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+    this.sayName = sayName;
+  }
+  // sayName() 在构造外部定义，这解决了函数重复问题，但是弄乱了全局作用域，这个问题可以通过原型模式来解决
+  function sayName() {
+    console.log("new name is " + this.name);
+  }
+  person1 = new Person("Nicholas", 29, "Software Engineer")
+  person2 = new Person("Greg", 27, "Doctor");
+
+  person1.sayName(); // "new name is Nicholas"
+  person2.sayName(); // "new name is Greg"
+}
+
+// ! 原型模式 —— 每个函数都会创建一个 prototype 属性，该属性是一个包含特定引用类型的实例共享的属性和方法的对象
+/* 这个对象就是通过构造函数创建的对象的原型。使用原型对象的好处是在它上面定义的属性和方法可以被对象实例共享。 */
+if (!true) {
+  function Person() {}; // or let Person = function() {};
+
+  /* 使用原型模式定义的属性和方法是所有实例共享的。 */
+  // * prototype
+  Person.prototype.name = "Nicholas";
+  Person.prototype.age = 29;
+  Person.prototype.job = "Software Engineer";
+  Person.prototype.sayName = function() {
+    console.log(this.name);
+  };
+
+  let person1 = new Person();
+  let person2 = new Person();
+  person1.sayName(); // "Nicholas"
+  person2.sayName(); // "Nicholas"
+  console.log(person1.sayName === person2.sayName); // true
+}
+
+// ? 理解原型 —— 只要创建一个函数，就会按照特定的规则为这个函数创建一个 prototype 属性（指向原型对象）
+/* 默认情况下，所有原型对象自动获得一个名为 constructor 的属性，指回与之关联的构造函数。对前面的例子而言，
+  Person.prototype.constructor 指向 Person。因构造函数而异可能会给原型对象添加其他属性和方法。
+   在自定义构造函数时，原型对象默认只会获得 constructor 属性，其他的所有方法都继承自 Object。每次调用构造函数创建
+  一个新实例，这个实例的内部 [[Prototype]] 指针就会被赋值为构造函数的原型对象。脚本中没有访问这个 [[Prototype]]
+  特性的标准方式，但 Firefox、Safari 和 Chrome 会在每个对象实例上暴露 __proto__ 属性，可以通过它访问对象的原
+  型。
+  */
+// * 关键在于理解一点：实例与构造函数原型之间有直接的联系，但实例与构造函数之间没有
+if (!true) {
+  /**
+   * 构造函数可以是函数表达式也可以是函数声明，因此以下两种形式都可以：
+   *      function Person() {};
+   *      let Person = function() {};
+   */
+  function Person() {};
+
+  // 声明之后，构造函数就有了一个与之关联的原型对象，这个对象是一个 Object 的实例
+  console.log(typeof Person.prototype, Person.prototype); // object, {constructor: ƒ}
+
+  // * prototype, constructor
+  // 构造函数有一个 prototype 属性引用其原型对象，而这个原型对象也有一个 constructor 属性引用这个构造函数
+  console.log(Person.prototype.constructor === Person); // true
+
+  // 正常的原型链都会终止于 Object 的原型对象，Object 原型的原型是 null
+  console.log(Person.prototype.__proto__ === Object.prototype); // true
+  console.log(Person.prototype.__proto__.constructor === Object); // true
+  console.log(Person.prototype.__proto__.__proto__ === null); // true
+
+  console.log(Person.prototype.__proto__); // {constructor: ƒ, __defineGetter__: ƒ, __defineSetter__: ƒ, hasOwnProperty: ƒ, __lookupGetter__: ƒ, …}
+
+  let person1 = new Person(), person2 = new Person();
+
+  /* 构造函数、原型对象和实例是 3 个完全不同的对象 */
+  console.log(person1 !== Person, person1 !== Person.prototype); // true, true
+  console.log(Person.prototype !== Person); // true
+
+  // * __proto__
+  /**
+   * 实例通过 __proto__ 链接到原型对象，它实际上指向隐藏特性 [[Prototype]] 构造函数
+   * 构造函数通过 prototype 属性链接到原型对象
+   * 实例与构造函数没有直接联系，与原型对象有直接联系
+   */
+  console.log(person1.__proto__ === Person.prototype); // true
+  console.log(person1.__proto__.constructor === Person); // true
+
+  // 同一个构造函数的两个实例共享同一个原型对象 
+  console.log(person1.__proto__ === person2.__proto__); // true
+
+  // * instanceof —— 检查实例的原型链中是否包含指定构造函数的原型
+  console.log(person1 instanceof Person, person1 instanceof Object); // true, true
+  console.log(Person.prototype instanceof Object); // true
+
+  /**
+   * Person.prototype 指向原型对象，而 Person.prototype.constructor 指回 Person 构造函数。原型对象包含
+   * constructor 属性和其他后来添加的属性。Person 的两个实例 person1 和 person2 都只有一个内部属性指回
+   * Person.prototype，而且两者都与构造函数没有直接联系。虽然这两个实例都没有属性和方法，但 person1.sayName() 
+   * 可以正常调用，这是因为对象属性查找机制的原因。
+   * 
+   *          <--------------------------------------------------------
+   *          ⬇                                                      |
+   *        Person            |--------->  Person Prototype           | 
+   *      prototype  -------->|         constructor    --------------->
+   *                          |         name          "Nicholas"
+   *                          |         age           29
+   *                          |         job           "Software Engineer"
+   *                          |         sayName       (function)
+   *                          |
+   *                          |<---------------------------------------
+   *        person1           |                                       |
+   *      [[Prototype]] ------>                     person2           |
+   *                                      [[Prototype]] -------------->
+   */
+
+  // * isPrototypeOf() —— 确定两个对象之间的关系
+  /* 本质上，isPrototypeOf() 会在传入参数的 [[Prototype]] 指向调用它的对象时返回 true，如下所示 */
+  console.log(Person.prototype.isPrototypeOf(person1)); // true
+
+  // * Object.getPrototypeOf() —— 返回参数的内部特性 [[Prototype]] 的值 
+  console.log(Object.getPrototypeOf(person1) === Person.prototype); // true
+  Person.prototype.name = "Prototype name"
+  console.log(Object.getPrototypeOf(person1).name); // "Prototype name"
+
+  // * Object.create() —— 创建一个新对象并为其制定原型
+  let biped = {numLegs: 2};
+  let person = Object.create(biped);
+  person.name = "Matt";
+  console.log(person.name, person.numLegs); // 'Matt' 2
+  console.log(Object.getPrototypeOf(person) === biped); // true
+}
+
+// ! 原型层级
+/* 在通过对象访问属性时，会按照这个属性的名称开始搜索。搜索开始于对象实例本身，如果在这个实例上发现了给定的名称，则返回
+  该名称对应的值。如果没有找到这个属性，则搜索会沿着指针进入原型对象，然后在原型对象上找到属性后，再返回对应的值。
+   虽然可以通过实例读取原型对象上的值，但不可能通过实例重写这个值，如果在实例上添加了一个与原型对象中同名的属性，那就会
+  在实例上创建这个属性，这个属性会遮住原型对象上的属性。*/
+if (!true) {
+  function Person() {};
+
+  Person.prototype.name = "Nicholas";
+  Person.prototype.age = 29;
+  Person.prototype.job = "Software engineer";
+  Person.prototype.sayName = function() {
+    console.log(this.name);
+  };
+
+  let person1 = new Person();
+  let person2 = new Person();
+
+  // * delete
+  /* 只要给对象实例添加一个属性，这个属性就会遮蔽原型对象上的同名属性，也就是虽然不会修改它，但会屏蔽对它的访问。即使
+    在实例上把这个属性设置为 null 也不会恢复它和原型的联系。不过使用 delete 操作符可以完全删除实例上的这个属性从而
+    让标识符解析过程能够继续搜索原型对象 */
+  person1.name = "Greg"; // 在实例上
+  console.log(person1.name, person2.name); // Greg Nicholas
+
+  delete person1.name;
+  console.log(person1.name); // "Nicholas"
+
+  // * hasOwnProperty() —— 确定某个属性是在实例上还是在原型对象上
+  /* 该方法继承自 Object，会在属性存在于调用它的对象实例上时返回 true */
+  console.log(person1.hasOwnProperty("name")); // false
+  person1.name = "Greg";
+  console.log(person1.name, person1.hasOwnProperty("name")); // "Greg" true
+  console.log(person2.name, person2.hasOwnProperty("name")); // "Nicholas" false
+  delete person1.name;
+  console.log(person1.name, person1.hasOwnProperty("name")); // "Nicholas" false
+}
+
+// ! 原型和 in 操作符
+/* 有两种方式使用 in 操作符：单独使用和在 for-in 循环中使用。在单独使用时，in 操作符会在可以通过对象访问指定属性时
+  返回 true，无论该属性是在实例上还是在原型上。 */
+if (!true) {
+  function Person() {};
+
+  Person.prototype.name = "Nicholas";
+  Person.prototype.age = 29;
+  Person.prototype.job = "Software engineer";
+  Person.prototype.sayName = function() {
+    console.log(this.name);
+  };
+
+  // * hasOwnProperty()
+  let person1 = new Person(), person2 = new Person();
+  console.log(person1.hasOwnProperty("name"), "name" in person1); // false true
+  person1.name = "Greg";
+  console.log(person1.name); // "Greg" 来自实例
+  console.log(person1.hasOwnProperty("name"), "name" in person1); // true true
+  delete person1.name;
+  console.log(person1.name); // "Nicholas" 来自原型
+  console.log(person1.hasOwnProperty("name"), "name" in person1); // false true
+
+  /* 上面 name 随时可以通过实例或通过原型访问到，因此调用 "name" in person1 始终返回 true，无论这个属性是否在实
+    例上，如果要确定某个属性是否存在于原型上，则可以像下面这样同时使用 hasOwnProperty() 和 in 操作符 */
+  function hasPrototypeProperty(object, name) {
+    /* 只要通过对象可以访问，in 操作符就返回 true，而 hasOwnProperty() 只有属性存在于实例上才返回 true。因此只要
+      in 返回 true 且 hasOwnProperty() 返回 false，就说明该属性是一个原型属性 */
+    return !object.hasOwnProperty(name) && (name in object);
+  }
+  let person = new Person();
+  console.log(hasPrototypeProperty(person, "name")); // true
+  person.name = "Greg";
+  console.log(hasPrototypeProperty(person, "name")); // false 
+
+  // * 在 for-in 中使用 in 操作符时，可以通过对象访问且可以被枚举的属性都会返回，包括实例属性和原型属性
+  /* 遮蔽原型中不可枚举（[[Enumerable]]）属性的实例属性也会在 for-in 循环中返回，因为默认情况下开发者定义的属性都
+    是可枚举的。要获得对象上所有可枚举的实例属性，可以使用 Object.keys() 方法，这个方法接收一个对象作为参数，返回包
+    含该对象所有可枚举属性名称的字符串数组 */
+  // * Object.keys(), Object.getOwnPropertyNames()
+  let keys = Object.keys(Person.prototype);
+  console.log(keys); // ['name', 'age', 'job', 'sayName']
+  let p1 = new Person();
+  p1.name = "Rob";
+  p1.age = 31;
+  let p1keys = Object.keys(p1);
+  console.log(p1keys); // ['name', 'age']
+  // 如果想列出 p1 所有的实例属性，无论是否可以枚举，都可以使用 Object.getOwnPropertyNames()
+  keys = Object.getOwnPropertyNames(Person.prototype);
+  console.log(keys); // ['constructor', 'name', 'age', 'job', 'sayName']
+
+  // ? 属性枚举顺序
+  /* for-in 循环、Object.keys()、Object.getOwnPropertyNames()、Object.getOwnpropertySymbols() 以及
+    Object.assign() 在属性枚举顺序方面有很大区别。for-in 循环和 Object.keys() 的枚举顺序是不确定的，取决于 JS
+    引擎。
+     Object.getOwnPropertyNames()、Object.getOwnPropertySymbols() 和 Object.assign() 的枚举顺序是确定的，
+    先以升序枚举数值健，然后以插入顺序枚举字符串和符号健。在对象字面量中定义的健以它们逗号分隔的顺序插入 */
+  let k1 = Symbol('k1'), k2 = Symbol('k2');
+  let o = {
+    1: 1,
+    first: 'first',
+    [k1]: 'sym2',
+    second: 'second',
+    0: 0
+  };
+  o[k2] = 'sym2';
+  o[3] = 3;
+  o.third = 'third';
+  o[2] = 2;
+  console.log(Object.getOwnPropertyNames(o)); // ['0', '1', '2', '3', 'first', 'second', 'third']
+  console.log(Object.getOwnPropertySymbols(o)); // [Symbol(k1), Symbol(k2)]
+}
+
+// ! 对象迭代
+// * Object.values(), Object.entries() —— 接收一个对象，返回它们内容的数组
+/* Object.values() 返回对象值的数组，Object.entries() 返回健/值对的数组 */
+if (true) {
+  let o = {
+    foo: "bar",
+    baz: 1,
+    qux: {}
+  };
+  console.log(Object.values(o)); // ['bar', 1, {…}]
+  console.log(Object.entries(o)); // [['foo', 'bar'], ['baz', 1], ['qux', {}]]
+
+  // 非字符串属性会被转换为字符串输出，且这两个方法执行对象的浅复制
+  const t = {
+    qux: {}
+  };
+  console.log(Object.values(t)[0] === t.qux); // true
+  console.log(Object.entries(t)[0][1] === t.qux); // true
+
+}
--- 5. Objects, classes and object-oriented programming/Object.js
+++ 5. Objects, classes and object-oriented programming/Object.js
@@ -3,16 +3,321 @@
  */
 
 // ! 创建自定义对象的通常方式是通过对象字面量的方式
-let person = {
-  name: "Nicholas",
-  age: 29,
-  job: "Software Engineer",
-  sayName() {
-    console.log(this.name);
-  }
-};
+if (false) {
+  let person = {
+    name: "Nicholas",
+    age: 29,
+    job: "Software Engineer",
+    sayName() {
+      console.log(this.name);
+    },
+  };
+}
 
 // ! 属性的类型 —— ES 使用一些内部特性来描述属性的特征
 // ? 数据属性 —— 包含一个保存数据值的位置。值会从这给值读取，也会写入到这个位置
 // * [[Configurable]] —— 属性是否可以通过 delete 删除并重新定义，是否可以修改它的特性以及是否可以把它改成访问器属性
 // * 默认为 true
+// * [[Enumerable]] —— 属性是否可以通过 for-of 循环返回，默认为 true
+// * [[Writable]] —— 属性是否可以被修改，默认为 true
+// * [[Value]] —— 属性实际的值，默认为 undefined
+
+// * Object.defineProperty() —— 修改属性的默认特性
+/* 接收三个参数：要添加属性的对象、属性名和描述符对象 */
+if (false) {
+  let person = {};
+  Object.defineProperty(person, "name", {
+    writable: false,
+    value: "Nicholas",
+    configurable: true,
+    enumerable: true,
+  });
+  console.log(person.name); // "Nicholas"
+  person.name = "Greg";
+  console.log(person.name); // "Nicholas"
+}
+
+// ? 访问器属性 —— 包含一个 getter 函数和一个 setter 函数
+// * [[Get]] —— 获取函数，在读取属性时调用，默认值为 undefined
+// * [[Set]] —— 设置函数，在写入属性时使用，默认值为 undefined
+// TODO 代码片段
+
+// TODO 定义多个属性 defineProperties
+
+// TODO 混入对象
+
+// TODO 对象标识及相等判定
+
+// ! 增强的对象语法
+// ? 属性值简写
+if (false) {
+  let name = "Matt";
+  person = {
+    name: name,
+  };
+  console.log(person); // {name: 'Matt'}
+}
+// * 给对象添加变量时，如果属性名和变量名相同，则可使用简写语法
+if (false) {
+  person = {
+    name,
+  };
+  console.log(person); // {name: 'Matt'}
+}
+
+// * 代码压缩程序会在不同作用域间保留属性名以防找不到引用
+if (false) {
+  function makePerson(name) {
+    return { name }; // 即使标识符只限定于函数作用域，编译器也会保留初始的标识符
+  }
+  person = makePerson("Matt");
+  console.log(person.name); // Matt
+}
+/* 如果使用 Google Closure 编译器压缩，那么函数参数会被缩短，而属性名不变： */
+if (false) {
+  function makePerson(a) {
+    return { name: a };
+  }
+  person = makePerson("Matt");
+  console.log(person.name); // Matt
+}
+
+// ? 可计算属性
+/* 在引入可计算属性之前，如果想使用变量的值作为属性，必须先声明对象，然后使用中括号语法来添加属性。换句话说，不能再对象
+  字面量中直接动态命名属性。 */
+if (false) {
+  const nameKey = "name";
+  const ageKey = "age";
+  const jobKey = "job";
+
+  let person = {};
+  person[nameKey] = "Matt";
+  person[ageKey] = 27;
+  person[jobKey] = "Software engineer";
+  console.log(person); // {name: 'Matt', age: 27, job: 'Software engineer'}
+}
+
+// * 有了可计算属性，就可以在对象字面量中完成动态属性赋值。JS 将中括号包围的内容当作表达式求值
+if (false) {
+  const nameKey = "name";
+  const ageKey = "age";
+  const jobKey = "job";
+
+  let person = {
+    [nameKey]: "Does",
+    [ageKey]: 28,
+    [jobKey]: "Software engineer",
+  };
+  console.log(person); // {name: 'Does', age: 28, job: 'Software engineer'}
+}
+
+// * 因为被当作 JS 表达式求值，所以可计算属性本身可以是复杂的表达式，在实例化时求值
+if (false) {
+  let uniqueToken = 0;
+  function getUniqueKey(key) {
+    return `${key}_${uniqueToken++}`;
+  }
+  let person = {
+    [getUniqueKey(nameKey)]: "Matt",
+    [getUniqueKey(ageKey)]: 27,
+    [getUniqueKey(jobKey)]: "Software engineer",
+  };
+  console.log(person); // {name_0: 'Matt', age_1: 27, job_2: 'Software engineer'}
+}
+
+// ? 简写方法名
+/* 再给对象定义方法时，通常都要写一个方法，以前的写法时这样的 */
+if (false) {
+  let person = {
+    sayName: function (name) {
+      console.log(`My name is ${name}`);
+    },
+  };
+  person.sayName("Matt"); // My name is Matt
+}
+
+// * 简写的方法语法遵循同样的模式
+if (false) {
+  let person = {
+    sayName(name) {
+      console.log(`My new name is ${name}`);
+    },
+  };
+  person.sayName("Matt"); // My new name is Matt
+}
+
+// * 简写方法名对 getter 函数和 setter 函数也适用
+if (false) {
+  let person = {
+    name_: "",
+    get name() {
+      return this.name_;
+    },
+    set name(name) {
+      this.name_ = name;
+    },
+    sayName() {
+      console.log(`My name is ${this.name_}`);
+    },
+  };
+  person.name = "Matt";
+  person.sayName(); // My name is Matt
+}
+
+// * 简写方法名可与计算属性健相互兼容
+if (false) {
+  const methodKey = "sayName";
+  let person = {
+    [methodKey](name) {
+      console.log(`My name is ${name}`);
+    },
+  };
+  person.sayName("Matt"); // Myu name is Matt
+}
+
+// ! 对象解构
+/* ES6 的对象解构语法可以在一条语句中使用嵌套数据实现一个或多个赋值操作。简单的说，对象解构就是使用与对象匹配的结构来
+  实现对象属性赋值 */
+
+/* 不使用对象解构 */
+if (!true) {
+  let person = {
+  name: "Matt",
+  age: 27,
+  };
+  let personName = person.name,
+    personAge = person.age;
+  console.log(personName, personAge); // Matt 27
+}
+
+// ? 使用对象解构
+if (!true) {
+  let person = {
+    name: 'Matt',
+    age: 27
+  };
+  let { name: personName, age: personAge } = person;
+  console.log(personName, personAge); // Matt 27
+}
+
+// * 如果想让变量直接使用属性名，则可以使用简写语法 
+if (!true) {
+  let person = {
+    name: 'Matt',
+    age: 27
+  };
+  let {name, age} = person;
+  console.log(name, age); // Matt 27
+}
+
+// * 解构赋值不一定与对象的属性匹配，赋值的时候可以忽略某些属性，而如果引用的属性不存在，则该变量的值就是 undefined 
+if (!true) {
+  let person = {
+    name: 'Matt',
+    age: 27
+  };
+  let {name, job} = person;
+  console.log(name, job); // Matt undefined
+}
+
+// *  也可以在解构赋值的同时定义默认值，适用于上面引用的属性不存在于源对象的情况
+if (!true) {
+  let person = {
+    name: 'Matt',
+    age: 27
+  };
+  let {name, job = 'Software engineer'} = person;
+  console.log(name, job); // 'Matt', 'Softwaare engineer'
+}
+
+// ? 解构在内部使用函数 ToObject()（无法在运行时环境中访问）把源数据结构转换为对象。
+/* 这意味着在对象解构的上下文中，原始值会被当成对象。根据 ToObject() 定义，null 和 undefined 不能被解构 */
+if (!true) {
+  let {length} = 'foobar';
+  let {constructor: c} = 4;
+  console.log(length, c === Number); // 6 true
+
+  let { _ } = null; // TypeError
+}
+
+// * 解构时如果给之前声明的变量赋值，则赋值表达式必须包含在一对括号中
+if (!true) {
+  let personName, personAge;
+  let person = {
+    name: 'Matt',
+    age: 27
+  };
+  ({name: personName, age: personAge} = person);
+  console.log(personName, personAge); // Matt 27
+}
+
+// ! 嵌套解构
+/* 解构对于引用嵌套的属性或赋值目标没有限制，因此可以通过解构来复制对象属性 */
+if (!true) {
+  let person = {
+    name: 'Matt',
+    age: 27,
+    job: {
+      title: 'Software engineer'
+    }
+  };
+  let personCopy = {};
+  ({
+    name: personCopy.name,
+    age: personCopy.age,
+    job: personCopy.job
+  } = person);
+
+  /* 因为一个对象的引用被赋值给 personCopy，所以修改 person.job 对象的属性也会影响 personCopy */
+  person.job.title = 'Hacker';
+
+  console.log(person, personCopy); // {name: 'Matt', age: 27, job: {{title: 'Hacker'}}} * 2
+
+  let {job: {title}} = person; // 声明 title 变量并将 person.job.title 的值赋给它
+  console.log(title); // Hacker
+}
+
+// ! 部分解构
+/* 涉及多个属性的解构赋值是一个输出无关的顺序化操作，如果一个解构表达式涉及多个赋值，开始的赋值成功而后面的赋值出错，则
+  整个解构赋值只会完成一部分 */
+if (!true) {
+  let person = {
+    name: 'Matt',
+    age: 27
+  };
+  let personName, personBar, personAge;
+  try {
+    // person.foo 是 undefined，因此会抛出错误
+    ({name: personName, foo: {bar: personBar}, age: personAge} = person);
+  } catch(e) {};
+
+  console.log(personName, personBar, personAge); // Matt, undefined, undefined
+}
+
+// ! 参数上下文匹配
+/* 在函数参数列表中也可以进行解构赋值，这不会影响 arguments 对象 */
+if (!true) {
+  let person = {
+    name: 'Matt',
+    age: 27
+  };
+
+  function printPerson(foo, {name, age}, bar) {
+    console.log(arguments);
+    console.log(name, age);
+  }
+
+  function printPerson2(foo, {name: personName, age: personAge}, bar) {
+    console.log(arguments);
+    console.log(personName, personAge);
+  }
+
+  printPerson('1st', person, '2nd');
+  // Arguments(3) ['1st', {…}, '2nd', callee: (...), Symbol(Symbol.iterator): ƒ]
+  // 'Matt' 27
+
+  printPerson2('1st', person, '2nd');
+  // Arguments(3) ['1st', {…}, '2nd', callee: (...), Symbol(Symbol.iterator): ƒ]
+  // 'Matt' 27
+}
+
